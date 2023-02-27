024df31fe289d22c1c57443951d3de353a01258a
iosbd <io232001@gmail.com>
2023-02-27 11:04:55 +0100

    Commit

--- src/it/develhope/otheroperators/testing.java
+++ src/it/develhope/otheroperators/testing.java
@@ -4,25 +4,12 @@ public class testing
 {
     public static void main (String arg[])
     {
-        int a=15+5-4;
-        //a=16
-        int b=a+1;
-        if (b %2==0)
-        {
-            System.out.println("the number is even");
-        }
-        else
-        {
-            System.out.println("the number is odd");
-        }
-        if (b*(b+1) %3==0)
-        {
-            System.out.println(true);
-        }
-        else
-        {
-            System.out.println(false);
-        }
+        int a=15;
+        int b=++a+1;
+        b %=2==0?++b:b;
+        int c=b*(b+1);
+        System.out.println(b%2!=0);
+        System.out.println(c%3==0);
     }
 
 }
